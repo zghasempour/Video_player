3ced6c02d70dc0e2b641ae65ee93f77db4279fa5
Zahra Ghasempour <ghasempour.zahra0@gmail.com>
2024-12-11 16:14:01 +0200

    Add player

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -6,20 +6,36 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-    private func findVideo(){
-       guard let path = Bundle.main.path(forResource: "test", ofType: "m4v") else{
-            
-            debugPrint("test.m4v not found")
-            
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
+    
+    private func playVideo(){
+        guard let path = Bundle.main.path(forResource: "video", ofType: "mp4") else{
+            debugPrint("video.mp4 not found")
             return
+        }
+        
+        let player = AVPlayer(url: (URL(fileURLWithPath: path)))
+        
+        let playController = AVPlayerViewController()
+        
+        playController.player = player
+        
+        present(playController, animated: true){
+            player.play()
         }
     }
 
